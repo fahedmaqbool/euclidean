0c51ccdde08f4b5c2ed2ffa83c27e197a7e7e1d2
Fahed Maqbool <fahma041@student.liu.se>
2019-09-21 17:32:12 +0200

    remove the error

--- uclidean/R/mainfile.R
+++ uclidean/R/mainfile.R
@@ -1,1 +1,12 @@
-print("chummi")
+
+#' Add together two numbers.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add2(1, 1)
+#' @export
+add2 <- function(x, y) {
+  x + y
+}
