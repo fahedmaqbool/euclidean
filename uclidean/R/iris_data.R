47e1738d20e4f38d26373f555d5cc437e07a047c
Fahed Maqbool <fahma041@student.liu.se>
2019-09-22 18:18:33 +0200

    added dataset in package

---
+++ uclidean/R/iris_data.R
@@ -0,0 +1,9 @@
+#' @docType data
+#' @usage data(iris)
+#' @format An object of class \code{".dataframe"}; see \code{\link[qtl]{read.cross}}.
+#' @param Sepal.Length description of the argument length
+#' @param Sepal.width description of the argument width
+#' @param Petal.Length description of the argument length
+#' @param Petal.width description of the argument width' 
+#' @param Species description of the argument species'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
